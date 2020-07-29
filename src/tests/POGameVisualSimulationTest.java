42a013d51db26b8c874c148bd430408a3941ec78
Florian Richoux <florian.richoux@polytechnique.edu>
2020-07-29 13:04:14 +0900

    Fix bug when we start with no workers

--- src/tests/POGameVisualSimulationTest.java
+++ src/tests/POGameVisualSimulationTest.java
@@ -43,7 +43,7 @@ public class POGameVisualSimulationTest {
 		//PhysicalGameState pgs = PhysicalGameState.load("maps/BroodWar/(4)BloodBath.scmB.xml", utt);
 		//PhysicalGameState pgs = PhysicalGameState.load("maps/8x8/FourBasesWorkers8x8.xml", utt);
 		//PhysicalGameState pgs = PhysicalGameState.load("maps/16x16/TwoBasesBarracks16x16.xml", utt);
-		//PhysicalGameState pgs = PhysicalGameState.load("maps/NoWhereToRun9x8.xml", utt);
+		PhysicalGameState pgs = PhysicalGameState.load("maps/NoWhereToRun9x8.xml", utt);
 		//PhysicalGameState pgs = PhysicalGameState.load("maps/DoubleGame24x24.xml", utt);
 
 		// microRTS competition hidden maps
@@ -59,7 +59,7 @@ public class POGameVisualSimulationTest {
 		//PhysicalGameState pgs = PhysicalGameState.load("maps/chambers32x32.xml", utt);
 
 		// not in the competition
-		PhysicalGameState pgs = PhysicalGameState.load("maps/12x12/basesWorkers12x12A.xml", utt);
+		//PhysicalGameState pgs = PhysicalGameState.load("maps/12x12/basesWorkers12x12A.xml", utt);
 		//PhysicalGameState pgs = PhysicalGameState.load("maps/24x24/basesWorkers24x24A.xml", utt);
 		//PhysicalGameState pgs = PhysicalGameState.load("maps/32x32/basesWorkers32x32A.xml", utt);
 		//PhysicalGameState pgs = PhysicalGameState.load("maps/64x64/basesWorkers64x64A.xml", utt);
